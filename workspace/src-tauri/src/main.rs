f082e27b1bb28a7b5b57c758b91f722eeb29c2ea
esthevann <esthevanferretti69@gmail.com>
2022-07-23 20:49:37 -0300

    starting move to prisma-client-rust, cargo workspaces, and removed tauri-plugin-sql

--- workspace/src-tauri/src/main.rs
+++ workspace/src-tauri/src/main.rs
@@ -3,21 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-use tauri_plugin_sql::{Migration, MigrationKind, TauriSql};
+
 use crate::request::send_request;
 mod request;
 
 fn main() {
     tauri::Builder::default()
-        .plugin(TauriSql::default().add_migrations(
-            "sqlite:test.db",
-            vec![Migration {
-                version: 1,
-                description: "create requests",
-                sql: include_str!("../migrations/1.sql"),
-                kind: MigrationKind::Up,
-            }],
-        ))
         .invoke_handler(tauri::generate_handler![greet, send_request])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
